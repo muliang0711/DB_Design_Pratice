a5d6157430ef881c046440bc0d02d4b80fe7ab48
lh-uni-pc <fake@mail.com>
2025-08-25 11:46:35 +0800

    separate file for seq, fix a prc, create script file to create all lh's tasks

---
+++ Bus_Station_managment_system/lh_task/createAllTask.sql
@@ -0,0 +1,18 @@
+-- Sequence
+@"Bus_Station_managment_system\lh_task\seq_,maintenanceID.sql"
+
+-- triggers
+@"Bus_Station_managment_system\lh_task\trg_on_bus_start_maintenance.sql"
+@"Bus_Station_managment_system\lh_task\trg_on_bus_end_maintenance.sql"
+
+-- procedures
+@"Bus_Station_managment_system\lh_task\prc_create_bus_maintenance.sql"
+@"Bus_Station_managment_system\lh_task\prc_list_cancelled_schedules.sql"
+
+-- reporting procedures
+@"Bus_Station_managment_system\lh_task\rpt_maintServiceRanking.sql"
+@"Bus_Station_managment_system\lh_task\rpt_maintStaffPerformance.sql"
+
+-- queries(views)
+@"Bus_Station_managment_system\lh_task\view_busMaintenanceStats.sql"
+@"Bus_Station_managment_system\lh_task\view_maintStaffRecentJob.sql"
