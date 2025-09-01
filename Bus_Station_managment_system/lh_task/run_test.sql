bfae7262656ed5a0cd6f1b32e55b93be41af70c6
Cookiecodess <142579195+Cookiecodess@users.noreply.github.com>
2025-09-01 18:23:37 +0800

    feat: add info output for both of lh's triggers

--- Bus_Station_managment_system/lh_task/run_test.sql
+++ Bus_Station_managment_system/lh_task/run_test.sql
@@ -9,6 +9,7 @@ SET SERVEROUTPUT ON
 -- INSERT INTO BusMaintenance (maintenanceID, busID) VALUES(1, 1);
 
 -- test prc_bus_start_maintenance
+-- NOTE: dont change these BusID (all belong to company C010) and StaffID (all are maintenance technicians). 
 -- BEGIN
 --     prc_create_bus_maintenance('B0305', 'S020', t_staffList('ST0002', 'ST0009'));
 --     prc_create_bus_maintenance('B0330', 'S015', t_staffList('ST0215', 'ST0365'));
@@ -16,5 +17,11 @@ SET SERVEROUTPUT ON
 -- END;
 -- /
 
-EXEC prc_list_cancelled_schedules('C010');
+-- EXEC prc_list_cancelled_schedules('C010');
+
+-- test trg_on_bus_end_maintenance
+-- maintenanceID may need changing, look at output of prc_bus_start_maintenance
+UPDATE BusMaintenance 
+SET status = 'completed'
+WHERE maintenanceID = 'M00502';
 
--- Bus_Station_managment_system/lh_task/trg_on_bus_end_maintenance.sql
+++ Bus_Station_managment_system/lh_task/trg_on_bus_end_maintenance.sql
@@ -1,23 +1,23 @@
 CREATE OR REPLACE TRIGGER trg_on_bus_end_maintenance 
 AFTER UPDATE OF status ON BusMaintenance
 FOR EACH ROW
--- DECLARE
-
 BEGIN
     IF :OLD.status = 'in_progress' AND :NEW.status = 'completed' THEN
-    -- Update bus's status to active
+        -- Update bus's status to active
         UPDATE Bus
         SET status = 'active',
             updatedAt = CURRENT_TIMESTAMP
         WHERE busID = :NEW.busID;
+        DBMS_OUTPUT.PUT_LINE('Bus ' || :NEW.busID || ' status set to active.');
 
-    -- Update affected driver list assignments' status to active
+        -- Update affected driver list assignments' status to active
         UPDATE DriverListAssignment dla
         SET status = 'active'
         WHERE dla.busID = :NEW.busID
         AND dla.assignedTo >= SYSDATE;
+        DBMS_OUTPUT.PUT_LINE(SQL%ROWCOUNT || ' driver assignments reactivated.');
 
-    -- Update affected route-driver assignments' status to active
+        -- Update affected route-driver assignments' status to active
         UPDATE RouteDriverAssignmentList rdal
         SET status = 'active'
         WHERE EXISTS (
@@ -26,8 +26,7 @@ BEGIN
             WHERE dla.assignmentID = rdal.assignmentID
             AND dla.busID = :NEW.busID
         );
-        END IF;
--- EXCEPTION
-
+        DBMS_OUTPUT.PUT_LINE(SQL%ROWCOUNT || ' route-driver assignments reactivated.');
+    END IF;
 END;
 /
